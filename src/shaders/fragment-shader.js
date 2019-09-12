052fe4f66f9213ce6494aea4876d66e38977396f
Nyannazz <kantemir.imam@gmail.com>
2019-09-12 11:45:58 +0200

    messy webgl experiments

--- src/shaders/fragment-shader.js
+++ src/shaders/fragment-shader.js
@@ -7,6 +7,13 @@ uniform sampler2D u_image0;
 uniform sampler2D u_image1;
 
 
+
+vec2 mirrored(vec2 vecIn) {
+  vec2 mod = mod(vecIn, 2.0);
+  return mix(mod, 2.0 - mod, step(1.0, mod));
+}
+
+
 float texTransition(vec2 pos, float limit, float delay){
 
 
@@ -33,12 +40,15 @@ void main() {
   float texOneModY=v_texCoord.y+u_scrollVal>1.0?1.0 - mod(v_texCoord.y+(u_scrollVal),1.0) : mod(v_texCoord.y+(u_scrollVal),1.0);
   // texture 2 starts flipped and gets flipped back on scroll
   float texTwoModY=v_texCoord.y+u_scrollVal<1.0?1.0 - mod(v_texCoord.y+(u_scrollVal),1.0) : mod(v_texCoord.y+(u_scrollVal),1.0);
-
-
-
   
-  vec4 color0 = texture2D(u_image0, vec2(v_texCoord.x, texOneModY));
-  vec4 color1 = texture2D(u_image1, vec2(v_texCoord.x, texTwoModY));
+  /* vec4 color0 = texture2D(u_image0, vec2(v_texCoord.x, texOneModY));
+  vec4 color1 = texture2D(u_image1, vec2(v_texCoord.x, texTwoModY)); */
+
+  vec2 mirrorTexCoordOne=mirrored(vec2(v_texCoord.x+u_scrollVal*2.0,v_texCoord.y+u_scrollVal));
+  vec2 mirrorTexCoordTwo=mirrored(vec2(v_texCoord.x+u_scrollVal,v_texCoord.y-u_scrollVal*2.0));
+
+  vec4 color0 = texture2D(u_image0, mirrorTexCoordOne);
+  vec4 color1 = texture2D(u_image1, mirrorTexCoordTwo);
 
 
 
@@ -46,12 +56,14 @@ void main() {
   //gl_FragColor = mix(color0, color1, ((v_texCoord.x+v_texCoord.y)/2.0)-u_scrollVal);
   
   /* vec4 red=vec4(1.0,0.0,0.0,1.0);
-  vec4 green=vec4(0.0,1.0,0.0,1.0); */
+  vec4 green=vec4(0.0,1.0,0.0,1.0);
+  gl_FragColor = mix(red, green, texTransition(v_texCoord, u_scrollVal, 0.2)); */
+  
+ 
 
   /* REAL */
   gl_FragColor = mix(color0, color1, texTransition(v_texCoord, u_scrollVal, 0.2));
 
-  //gl_FragColor = mix(red, green, (v_texCoord.y+u_scrollVal)*0.0);
 
 
 }
